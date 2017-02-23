235cc82a5badf2c5173e60a2fb48c79260f80887
mtnview101 <mtnview101@gmail.com>
2017-02-23 13:57:28 -0800

    commit 6

--- src/test/java/core/ElementsValidationTest2.java
+++ src/test/java/core/ElementsValidationTest2.java
@@ -12,14 +12,18 @@ import org.openqa.selenium.htmlunit.*;
 import org.testng.*;
 import org.testng.annotations.*;
 import java.lang.reflect.Method;
+import org.testng.asserts.SoftAssert;
 
-public class ElementsValidationTest implements ITest {
+public class ElementsValidationTest2 implements ITest {
 	static String in_browser = "Edge"; // "HtmlUnit" "Firefox" "Chrome"  "Safari"  "IE"  "Edge"
     static WebDriver driver;
     static final String baseUrl = "http://alex.academy/exercises/signup/v1/";
+    static String urlOld="1";
 
-    String csvFile = "./src/resources/test_data/csv/bat/ev.properties";
-    //String csvFile = "./src/resources/test_data/csv/bat/elements_validation_6.csv";
+    //String csvFile = "./src/resources/test_data/csv/bat/ev.properties";
+    //String csvFile = "./src/resources/test_data/csv/bat/elements_validation_chrome.csv";
+    String csvFile = "./src/resources/test_data/csv/bat/elements_validation_3.csv";
+    SoftAssert s_assert = new SoftAssert();
     
        private String test_name = "";
        public String getTestName() {return test_name;}
@@ -39,27 +43,29 @@ public class ElementsValidationTest implements ITest {
               ArrayList<String[]> al = new ArrayList<>();
               BufferedReader br = new BufferedReader(new FileReader(csvFile));
               while ((cvsLine = br.readLine()) != null) {
+            	     //System.out.println("cvsLine: "+cvsLine);
                      a = cvsLine.split(";");
+                     //System.out.println("line: "+a);
                      al.add(a);}
               br.close();
               return al.iterator();}
-
        
        @Override
        @Test(dataProvider = "dp")
        public void test(String tc_id, String url, String element_id, String element_size, String element_location) throws IOException {
 System.out.println(tc_id);
-System.out.println(url);
-System.out.println(element_id);
-System.out.println(element_size);
-System.out.println(element_location);
+              if (!url.equals(urlOld)) {getDriver(url); urlOld=url;System.out.println("urlOld="+urlOld+" url="+url);} 
+/*            s_assert.assertEquals(isPresent(element_id, driver), equalTo(true));
+              s_assert.assertEquals(size(element_id, driver), equalTo(element_size));
+              s_assert.assertEquals(location(element_id, driver), equalTo(element_location));}}*/
+            	  System.out.println(tc_id);
 
-              getDriver(url);
-              assertThat(isPresent(element_id, driver), equalTo(true));
-              assertThat(size(element_id, driver), equalTo(element_size));
-              assertThat(location(element_id, driver), equalTo(element_location));}
+            	                //getDriver(url);
+            	                assertThat(isPresent(element_id, driver), equalTo(true));
+            	                assertThat(size(element_id, driver), equalTo(element_size));
+            	                assertThat(location(element_id, driver), equalTo(element_location));}
 
-       @AfterMethod 
+       @AfterTest
        public void am() {driver.quit();}
 
        public static void getDriver(String url) throws IOException {
@@ -86,3 +92,4 @@ System.out.println(element_location);
                   String l = driver.findElement(By.id(element_id)).getLocation().toString(); return l;}
              else {return n;}}
 }
+
