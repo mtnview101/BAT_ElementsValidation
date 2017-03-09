4c7fdd5f9ce06285d9a3689241ca75f0ad410b34
mtnview101 <mtnview101@gmail.com>
2017-03-08 20:34:45 -0800

    commit 8

--- src/main/java/core/Browsers.java
+++ src/main/java/core/Browsers.java
@@ -19,33 +19,49 @@ import org.openqa.selenium.safari.SafariOptions;
 
 public class Browsers {
 	static WebDriver driver;
+	private static final String mac="MAC"; // mac
+	static final String win="WINDOWS"; // windows
+	static final String os="os.name"; // OS name
+	static final String driverPathEdge = "./src/main/resources/webdrivers/pc/MicrosoftWebDriver.exe";
 
 	public static void setWebDriver(String browser) throws IOException {
         Logger logger = Logger.getLogger("");
         logger.setLevel(Level.OFF);
         String driverPath = ""; // project folder with browsers drivers
+
         
-  if ((browser == "Firefox") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+  if ("Firefox".equals(browser) && System.getProperty(os).toUpperCase().contains(mac)) 
+/*	  Multiple markers at this line
+		- When doing a String.toLowerCase()/toUpperCase() call, use a Locale
+		- Useless parentheses.
+		- Potential violation of Law of Demeter (method chain calls)
+		- The String literal mac appears 6 times in this file; the first occurrence is 
+		  on line 28
+		- The String literal os appears 12 times in this file; the first occurrence is 
+		 on line 28*/
         {driverPath = "./src/main/resources/webdrivers/mac/geckodriver.sh";}
-else if ((browser == "Firefox") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+else if (("Firefox".equals(browser)) && (System.getProperty(os).toUpperCase().contains(win))) 
         {driverPath = "./src/main/resources/webdrivers/pc/geckodriver.exe";}
-else if ((browser == "Chrome") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+else if (("Chrome".equals(browser)) && (System.getProperty(os).toUpperCase().contains(mac))) 
         {driverPath = "./src/main/resources/webdrivers/mac/chromedriver";}
-else if ((browser == "Chrome") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+else if (("Chrome".equals(browser)) && (System.getProperty(os).toUpperCase().contains(win))) 
         {driverPath = "./src/main/resources/webdrivers/pc/chromedriver.exe";}
-else if ((browser == "Safari") && (System.getProperty("os.name").toUpperCase().contains("MAC")))
+else if (("Safari".equals(browser)) && (System.getProperty(os).toUpperCase().contains(mac)))
        {}
-else if ((browser == "Safari") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+else if (("Safari".equals(browser)) && (System.getProperty(os).toUpperCase().contains(win))) 
        {throw new IllegalArgumentException("Safari is not available for Windows");}
-else if ((browser == "IE") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+else if (("IE".equals(browser)) && (System.getProperty(os).toUpperCase().contains(mac))) 
        {throw new IllegalArgumentException("Internet Explorer is not available for macOS");}
-else if ((browser == "IE") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS"))) 
+else if (("IE".equals(browser)) && (System.getProperty(os).toUpperCase().contains(win))) 
        {driverPath = "./src/main/resources/webdrivers/pc/IEDriverServer.exe";}
-else if ((browser == "Edge") && (System.getProperty("os.name").toUpperCase().contains("MAC"))) 
+else if (("Edge".equals(browser)) && (System.getProperty(os).toUpperCase().contains(mac))) 
        {throw new IllegalArgumentException("Microsoft Edge is not available for macOS");}
-else if ((browser == "Edge") && (System.getProperty("os.name").toUpperCase().contains("WINDOWS")))
-       {driverPath = "./src/main/resources/webdrivers/pc/MicrosoftWebDriver.exe";}
-else if (browser == "HtmlUnit") 
+else if (("Edge".equals(browser)) && (System.getProperty(os).toUpperCase().contains(win)))
+       {driverPath = driverPathEdge;}
+else if ("HtmlUnit".equals(browser)) //Use equals() to compare strings instead of '==' or '!='
+									//Position literals first in String comparisons - 
+									//that way if the String is null you won't get 
+									//a NullPointerException, it'll just return false.
 		  {}
 else   {throw new IllegalArgumentException("Unknown OS");}
 
@@ -62,9 +78,9 @@ case "Chrome":
        System.setProperty("webdriver.chrome.driver", driverPath);
        System.setProperty("webdriver.chrome.silentOutput", "true");
        ChromeOptions option = new ChromeOptions();
-       if (System.getProperty("os.name").toUpperCase().contains("MAC")) 
+       if (System.getProperty(os).toUpperCase().contains(mac)) 
        {option.addArguments("-start-fullscreen");}
-       else if (System.getProperty("os.name").toUpperCase().contains("WINDOWS")) 
+       else if (System.getProperty(os).toUpperCase().contains(win)) 
        {option.addArguments("--start-maximized");}
        else {throw new IllegalArgumentException("Unknown OS");}
        driver = new ChromeDriver(option);
